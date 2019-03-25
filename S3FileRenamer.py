b9f26e0d5f69fdeca85037a11ff27ab13958314b
limenVTech <limen@vt.edu>
2019-03-25 10:17:39 -0400

    changed imports and line spacing

--- S3FileRenamer.py
+++ S3FileRenamer.py
@@ -6,9 +6,11 @@
     Default behavior is to only generate the log file for review.  Pass the "--rename" or "-r" flag to rename files.
  """
 
-import os
+from os import rename, walk
+from os.path import dirname, basename, exists, join
 import argparse
 import re
+
 
 def contains_bad_characters(string, badchars):
     """Search input string for illegal characters.  Return True, list of characters found, if any found."""
@@ -20,6 +22,7 @@ def contains_bad_characters(string, badchars):
         return True, found_bad_characters
     else:
         return False, ""
+
 
 def perform_rename(old_name, root_path, found_bad_characters, rename_do):
     """Remove illegal characters if rename argument was passed to program."""
@@ -35,18 +38,19 @@ def perform_rename(old_name, root_path, found_bad_characters, rename_do):
     done = False
     check_name = corrected_name
     while not done:
-        if os.path.exists(os.path.join(root_path, check_name)):
+        if exists(join(root_path, check_name)):
             n += 1
             check_name = re.sub(check_name.split(".")[0], corrected_name.split(".")[0] + str(n), check_name)
         else:
             done = True
             corrected_name = check_name
     if rename_do:
-        print("Renaming {} to {}".format(os.path.join(root_path, old_name), os.path.join(root_path, corrected_name)))
-        os.rename(os.path.join(root_path, old_name), os.path.join(root_path, corrected_name))
+        print("Renaming {} to {}".format(join(root_path, old_name), join(root_path, corrected_name)))
+        rename(join(root_path, old_name), join(root_path, corrected_name))
     else:
         print(corrected_name)
     return corrected_name
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find and replace S3-illegal characters")
@@ -57,16 +61,16 @@ if __name__ == "__main__":
     path = args["path"]
     do_rename = args["rename"]
     log_directory = args["log"]
-    log_file = open(os.path.join(log_directory, "rename.log"), "w")
+    log_file = open(join(log_directory, "rename.log"), "w")
     log_file.write("CHARACTERS || ORIGINAL NAME || NEW NAME\n")
     bad_characters = ["&", "$", "@", "=", ";", "+", ",", "?", "\\", "{", "^", "}", "%", "`", "]", "\"", ">", "[", "~",
                       "<", "#", "|"]
-    for root, dirs, files in os.walk(path, topdown=False):
+    for root, dirs, files in walk(path, topdown=False):
         for name in files:
             found, characters_found = contains_bad_characters(name, bad_characters)
             if found is True:
                 new_name = perform_rename(name, root, characters_found, do_rename)
-                log_line = "{} || {} || {}\n".format(" ".join(characters_found), os.path.join(root, name), os.path.join(root, new_name))
+                log_line = "{} || {} || {}\n".format(" ".join(characters_found), join(root, name), join(root, new_name))
                 print(log_line)
                 log_file.write(log_line)
     log_file.close()
