c9ecdf5fcf8802038ac00b52ad6732ef79ab759a
Jim Tuttle <jim@braggtown.com>
2019-03-28 14:52:25 -0400

    Add rename and delete

--- S3FileRenamer.py
+++ S3FileRenamer.py
@@ -6,8 +6,9 @@
     Default behavior is to only generate the log file for review.  Pass the "--rename" or "-r" flag to rename files.
  """
 
-from os import rename, walk
-from os.path import dirname, basename, exists, join
+from os import rename, walk, remove
+from os.path import exists, join, splitext
+from shutil import rmtree, move
 import argparse
 import re
 
@@ -26,12 +27,10 @@ def contains_bad_characters(string, badchars):
 
 def perform_rename(old_name, root_path, found_bad_characters, rename_do):
     """Remove illegal characters if rename argument was passed to program."""
-# Replaced "re.sub" because of conflict with regular expression wildcards
-#   corrected_name = re.sub("|".join(found_bad_characters), "", old_name)
     corrected_name = ""
     for c in old_name:
         if c in found_bad_characters:
-            c = "_"                     # Replacement character can be ""
+            c = "_"
         corrected_name += c
     # Avoid overwriting files that already exist.
     n = 0
@@ -47,9 +46,50 @@ def perform_rename(old_name, root_path, found_bad_characters, rename_do):
     if rename_do:
         print("Renaming {} to {}".format(join(root_path, old_name), join(root_path, corrected_name)))
         rename(join(root_path, old_name), join(root_path, corrected_name))
-    else:
-        print(corrected_name)
     return corrected_name
+
+
+def is_temp_file(fname):
+    """Determine if file matches pattern of temp files"""
+    if fname.startswith("~$") or fname.startswith(".") or fname == "thumbs.db" or \
+            splitext(fname)[1].lower() == ".tmp":
+        return True
+    else:
+        return False
+
+
+def is_temp_dir(dname):
+    """Determine if directory matches pattern for temp directory"""
+    if dname.startswith("."):
+        return True
+    else:
+        return False
+
+
+def generate_log(log_directory, path):
+    log_file = open(join(log_directory, "rename.log"), "w")
+    log_file.write("ACTION||CHARACTERS||ORIGINAL NAME||NEW NAME||TYPE\n")
+    for root, dirs, files in walk(path, topdown=False):
+        for dir in dirs:
+            if is_temp_dir(dir):
+                log_line = "DELETE||''||{}||''||DIR\n".format(join(root, dir))
+                print(log_line)
+                log_file.write(log_line)
+        for name in files:
+            log_line = False
+            if is_temp_file(name):
+                log_line = "DELETE||''||{}||''||FILE\n".format(join(root, name))
+            else:
+                found, characters_found = contains_bad_characters(name, bad_characters)
+                if found:
+                    new_name = perform_rename(name, root, characters_found, do_rename)
+                    log_line = "RENAME||{}||{}||{}||FILE\n".format(" ".join(characters_found), join(root, name),
+                                                                   join(root, new_name))
+            if log_line:
+                print(log_line)
+                log_file.write(log_line)
+    log_file.close()
+    return join(log_directory, "rename.log")
 
 
 if __name__ == "__main__":
@@ -57,20 +97,36 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--rename", help="Perform rename action.", action="store_true", default=False)
     parser.add_argument("-p", '--path', help="Path to data directory", required=True)
     parser.add_argument("-l", '--log', help="Path to log file directory", required=True)
+    parser.add_argument("-d", "--delete", help="Delete temp files and directories.", action="store_true", default=False)
     args = vars(parser.parse_args())
     path = args["path"]
     do_rename = args["rename"]
+    do_delete = args["delete"]
     log_directory = args["log"]
-    log_file = open(join(log_directory, "rename.log"), "w")
-    log_file.write("CHARACTERS || ORIGINAL NAME || NEW NAME\n")
     bad_characters = ["&", "$", "@", "=", ";", "+", ",", "?", "\\", "{", "^", "}", "%", "`", "]", "\"", ">", "[", "~",
                       "<", "#", "|"]
-    for root, dirs, files in walk(path, topdown=False):
-        for name in files:
-            found, characters_found = contains_bad_characters(name, bad_characters)
-            if found is True:
-                new_name = perform_rename(name, root, characters_found, do_rename)
-                log_line = "{} || {} || {}\n".format(" ".join(characters_found), join(root, name), join(root, new_name))
-                print(log_line)
-                log_file.write(log_line)
-    log_file.close()
+    logfile = generate_log(log_directory, path)
+
+    if do_delete or do_rename:
+        log = open(logfile, "r")
+        for line in log:
+            line = line.strip()
+            print(line)
+            action, chars, original, new, objtype = line.split("||")
+            if do_delete and action == "DELETE":
+                print("DELETING {} {}".format(objtype, original))
+                if objtype == "DIR":
+                    rmtree(original)
+                if objtype == "FILE":
+                    try:
+                        remove(original)
+                    except FileNotFoundError:
+                        pass
+            if do_rename and action == "RENAME":
+                print("Moving {} {}".format(objtype, original))
+                try:
+                    move(original, new)
+                except FileNotFoundError:
+                    pass
+
+
